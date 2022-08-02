e8d7c355b5ae019eec9a188259527129de3d61f0
Fabio Vianna <fabio.vianna@FITCARD.infra.br>
2022-08-02 11:42:05 -0300

    tt

--- scrollreveal.js
+++ scrollreveal.js
@@ -1,54 +1,42 @@
+//-----------Biblioteca ScrollReveal-----------------------------------
+
 window.sr = ScrollReveal({ reset: true });
 
+ScrollReveal().reveal(".introSection", {
+  delay: 450,
+  origin: "top",
+  distance: "300px",
+  easing: "ease-in-out",
+  reset: false,
+});
 
-ScrollReveal().reveal('.introSection', 
-{   delay: 450,
-    origin : 'top',
-    distance : '300px',
-    easing   : 'ease-in-out',
-    reset    : false, }
-);
+ScrollReveal().reveal(".about", {
+  delay: 450,
+  origin: "top",
+  distance: "120px",
+  easing: "ease-in-out",
+  reset: true,
+});
 
-ScrollReveal().reveal('.about',
- { delay: 450,
-    origin : 'bottom',
-    distance : '120px',
-    easing   : 'ease-in-out',
-    reset    :true,
-    
+ScrollReveal().reveal(".skillsSection", {
+  delay: 700,
+  origin: "left",
+  distance: "120px",
+  easing: "ease-in-out",
+  reset: true,
+});
 
-}
-
-);
-
-ScrollReveal().reveal('.skillsSection',
- { delay: 700,
-    origin : 'left',
-    distance : '120px',
-    easing   : 'ease-in-out',
-    reset    :true,
-
-}
-
- );
-
-ScrollReveal().reveal('.portifolioSection',
- { delay:700,
-    origin : 'rigth',
-    distance : '120px',
-    easing   : 'ease-in-out',
-    reset    : true,
-
-}
-
-);
-ScrollReveal().reveal('.contactSection', 
-{ delay: 550,
-    origin : 'bottom',
-    distance : '120px',
-    easing   : 'ease-in-out',
-    reset    : true,
-
-}
-
-);
+ScrollReveal().reveal(".portifolioSection", {
+  delay: 700,
+  origin: "rigth",
+  distance: "120px",
+  easing: "ease-in-out",
+  reset: true,
+});
+ScrollReveal().reveal(".contactSection", {
+  delay: 550,
+  origin: "bottom",
+  distance: "120px",
+  easing: "ease-in-out",
+  reset: true,
+});
